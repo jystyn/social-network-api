7ec7f7ff6cf9983d9f573ba7ab02c062b4cb9a48
joostyn <justyn.helgeson@gmail.com>
2022-08-17 19:31:42 -0500

    Wrote models and downloaded moment for timestamps

---
+++ models/Thought.js
@@ -0,0 +1,55 @@
+const { Schema, model, SchemaTypes, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: SchemaTypes.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => moment(timestamp).format('MMMM Do YYYY, h:mm:ss a'),
+    }
+});
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        requred: true,
+        min: 1,
+        max: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => moment(timestamp).format('MMMM Do YYYY, h:mm:ss a'),
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+thoughtSchema.virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+module.exports = Thought;
